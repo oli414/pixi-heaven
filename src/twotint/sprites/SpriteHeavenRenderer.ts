c1fa477186e3ad13b737b0722b6b86d0951f42fb
oli414 <oli414@live.nl>
2019-02-24 19:52:08 +0100

    Replace twotint with hue shift

--- src/twotint/sprites/SpriteHeavenRenderer.ts
+++ src/twotint/sprites/SpriteHeavenRenderer.ts
@@ -31,13 +31,39 @@ varying vec4 vLight, vDark;
 varying float vTextureId;
 uniform sampler2D uSamplers[%count%];
 
+vec3 hueShift( vec3 color, float hueAdjust ){
+
+	const vec3  kRGBToYPrime = vec3 (0.299, 0.587, 0.114);
+	const vec3  kRGBToI      = vec3 (0.596, -0.275, -0.321);
+	const vec3  kRGBToQ      = vec3 (0.212, -0.523, 0.311);
+
+	const vec3  kYIQToR     = vec3 (1.0, 0.956, 0.621);
+	const vec3  kYIQToG     = vec3 (1.0, -0.272, -0.647);
+	const vec3  kYIQToB     = vec3 (1.0, -1.107, 1.704);
+
+	float   YPrime  = dot (color, kRGBToYPrime);
+	float   I       = dot (color, kRGBToI);
+	float   Q       = dot (color, kRGBToQ);
+	float   hue     = atan (Q, I);
+	float   chroma  = sqrt (I * I + Q * Q);
+
+	hue += hueAdjust;
+
+	Q = chroma * sin (hue);
+	I = chroma * cos (hue);
+
+	vec3    yIQ   = vec3 (YPrime, I, Q);
+
+	return vec3( dot (yIQ, kYIQToR), dot (yIQ, kYIQToG), dot (yIQ, kYIQToB) );
+
+}
+
 void main(void) {
 vec4 texColor;
 vec2 texCoord = vTextureCoord;
 float textureId = floor(vTextureId+0.5);
 %forloop%
-gl_FragColor.a = texColor.a * vLight.a;
-gl_FragColor.rgb = ((texColor.a - 1.0) * vDark.a + 1.0 - texColor.rgb) * vDark.rgb + texColor.rgb * vLight.rgb;
+gl_FragColor = vec4(hueShift(texColor.rgb, vLight.r), texColor.a);
 }`;
 
 		createVao(vertexBuffer: PIXI.glCore.GLBuffer) {
